f688f91753c9b73709cc2ff8e7747b4787e42a60
amin saffar <amin@stackadoc.com>
2024-08-14 07:14:06 +0200

    start creating first screen layouts

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,41 @@
+import { TextInput, View, StyleSheet } from "react-native";
+
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput maxLength={2} style={styles.numberInput} />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 21,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold'
+  }
+});
